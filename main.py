b94b40d9f1488ff5ac4558070237effcae782f5c
Tom Coleman <tom@cabranet.dev>
2024-12-02 12:36:25 +0100

    Refactor chatbot implementation: integrate rule-based and transformer-based chatbots
    - Updated main.py to support both rule-based and transformer-based chatbots.
    - Modified routes to include separate endpoints for each chatbot type.
    - Enhanced index.html to provide options for users to choose between chatbots.
    - Improved CSS styles for better UI/UX, including responsive design adjustments.
    - Removed outdated transformer model files to streamline the project structure.

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import ssl
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from rule_based.rule_based_v2 import Chatbot
+from rule_based.rule_based_v3 import Chatbot as RuleBasedChatbot
+from transformer_based.chat import TransformerChatbot
 
 try:
     _create_unverified_https_context = ssl._create_unverified_context
@@ -19,18 +20,41 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-# Initialize the Chatbot
-chatbot = Chatbot()
+# Initialize both chatbots
+rule_based_chatbot = RuleBasedChatbot(data_path='./processed_data/processed_conversations.pkl')
+transformer_chatbot = TransformerChatbot(model_path='./dialogue_model_final')
 
 @app.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/chat")
-async def chat(request: Request):
+@app.get("/rule-based")
+async def rule_based(request: Request):
+    return templates.TemplateResponse("rules.html", {"request": request})
+
+@app.get("/transformer-based")
+async def transformer_based(request: Request):
+    return templates.TemplateResponse("trans.html", {"request": request})
+
+@app.post("/chat/rule-based")
+async def chat_rule_based(request: Request):
     form_data = await request.form()
     user_message = form_data['message']
-    bot_response = chatbot.get_response(user_message)
+    bot_response = rule_based_chatbot.get_response(user_message)
+    return templates.TemplateResponse(
+        "chat_response.html", 
+        {
+            "request": request, 
+            "user_message": user_message, 
+            "response": bot_response
+        }
+    )
+
+@app.post("/chat/transformer-based")
+async def chat_transformer_based(request: Request):
+    form_data = await request.form()
+    user_message = form_data['message']
+    bot_response = transformer_chatbot.get_response(user_message)
     return templates.TemplateResponse(
         "chat_response.html", 
         {
